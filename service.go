677241c13e3ade41fb05cc5933cd432b9c114cb2
Joel Yoshiya Foster <jyfoster@wisc.edu>
2022-12-05 14:54:06 -0600

    renaming for clarity

--- service.go
+++ service.go
@@ -78,23 +78,16 @@ func main() {
 
 // Internal functions
 
-// These rules collectively define how many points should be awarded to a receipt.
-// One point for every alphanumeric character in the retailer name.
-// 50 points if the total is a round dollar amount with no cents.
-// 25 points if the total is a multiple of 0.25
-// 5 points for every two items on the receipt.
-// If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
-// 6 points if the day in the purchase date is odd.
-// 10 points if the time of purchase is after 2:00pm and before 4:00pm
+// Calculate points for receipt - based on ruleset given
 func processPoints(r Receipt) int {
 	// process points
 
 	// 1 point for every alphanumeric character in the retailer name.
 	// clean retailer name for non alphanumeric characters
 	// define regex for alphanumeric characters
-	regex := regexp.MustCompile("[^a-zA-Z0-9]+")
+	nonAlphaNumericRegex := regexp.MustCompile("[^a-zA-Z0-9]+")
 	// replace non alphanumeric characters with empty string
-	retailer := regex.ReplaceAllString(r.Retailer, "")
+	retailer := nonAlphaNumericRegex.ReplaceAllString(r.Retailer, "")
 	// count alphanumeric characters
 	retailerPoints := len(retailer)
 
